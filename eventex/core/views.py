b15bbaf1f1a6cee4301547e7874c7bdb52ac3d0b
Victor <vitimm144@gmail.com>
2014-02-05 21:11:17 -0200

    Refatorando o codigo para utilização do RequestContext.

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,10 +1,7 @@
 #coding: utf-8
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
-from django.template import loader, Context
-from django.conf import settings
-
+from django.template import RequestContext
 
 def home(request):
-    context = {'STATIC_URL' : settings.STATIC_URL}
+    context = RequestContext(request)
     return render_to_response('index.html', context)
