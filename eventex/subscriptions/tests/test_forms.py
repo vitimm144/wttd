507bd85f2f0e43982d2e60347a8e799fb1af3fdd
victor <vitimm144@gmail.com>
2014-03-08 13:33:07 -0300

    Forms features

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -26,10 +26,20 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(email='')
         self.assertFalse(form.errors)
 
+    def test_name_must_be_capitalized(self):
+        'Name must be capitalized.'
+        form = self.make_validated_form(name='HENRIQUE bastos')
+        self.assertEqual('Henrique Bastos', form.cleaned_data['name'])
+
     def make_validated_form(self, **kwargs):
         data = dict(name= 'Henrique Bastos', email='henrique@bastos.net',
-                    cpf='12345678901', phone='21-96186180')
+                    cpf='12345678901', phone_0='21', phone_1='96186180')
         data.update(kwargs)
         form = SubscriptionForm(data)
         form.is_valid()
         return form
+
+    def test_must_inform_email_or_phone(self):
+        'Email and Phone are optional, but one must be informed.'
+        form = self.make_validated_form(email='', phone_0='', phone_1='')
+        self.assertItemsEqual(['__all__'], form.errors)
